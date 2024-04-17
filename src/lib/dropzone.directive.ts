8508533ed8373143042640c8ce7703f4571833d4
Rafael Cruz <rafael@destra.software>
2024-04-17 11:24:07 -0300

    - fix for ssr

--- src/lib/dropzone.directive.ts
+++ src/lib/dropzone.directive.ts
@@ -1,11 +1,32 @@
-import { PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { NgZone, Inject, Optional, ElementRef, Renderer2, Directive,
-  OnInit, OnDestroy, DoCheck, OnChanges, Input, Output, EventEmitter,
-  SimpleChanges, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
+import {
+  Directive,
+  DoCheck,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  Input,
+  KeyValueDiffer,
+  KeyValueDiffers,
+  NgZone,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Optional,
+  Output,
+  PLATFORM_ID,
+  Renderer2,
+  SimpleChanges
+} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
-import { DROPZONE_CONFIG, DropzoneConfig, DropzoneConfigInterface,
-  DropzoneEvent, DropzoneEvents } from './dropzone.interfaces';
+import Dropzone from 'dropzone'
+import {
+  DROPZONE_CONFIG,
+  DropzoneConfig,
+  DropzoneConfigInterface,
+  DropzoneEvent,
+  DropzoneEvents
+} from './dropzone.interfaces';
 
 @Directive({
   selector: '[dropzone]',
@@ -13,7 +34,7 @@ import { DROPZONE_CONFIG, DropzoneConfig, DropzoneConfigInterface,
 })
 export class DropzoneDirective implements OnInit, OnDestroy, DoCheck, OnChanges {
   private instance: any;
-  private Dropzone: any;
+  private isBrowser = false;
 
   private configDiff: KeyValueDiffer<string, any> | null = null;
 
@@ -21,55 +42,51 @@ export class DropzoneDirective implements OnInit, OnDestroy, DoCheck, OnChanges 
 
   @Input('dropzone') config?: DropzoneConfigInterface;
 
-  @Output('init'                  ) DZ_INIT                     = new EventEmitter<any>();
+  @Output('init') DZ_INIT = new EventEmitter<any>();
 
-  @Output('error'                 ) DZ_ERROR                    = new EventEmitter<any>();
-  @Output('success'               ) DZ_SUCCESS                  = new EventEmitter<any>();
-  @Output('sending'               ) DZ_SENDING                  = new EventEmitter<any>();
-  @Output('canceled'              ) DZ_CANCELED                 = new EventEmitter<any>();
-  @Output('complete'              ) DZ_COMPLETE                 = new EventEmitter<any>();
-  @Output('processing'            ) DZ_PROCESSING               = new EventEmitter<any>();
+  @Output('error') DZ_ERROR = new EventEmitter<any>();
+  @Output('success') DZ_SUCCESS = new EventEmitter<any>();
+  @Output('sending') DZ_SENDING = new EventEmitter<any>();
+  @Output('canceled') DZ_CANCELED = new EventEmitter<any>();
+  @Output('complete') DZ_COMPLETE = new EventEmitter<any>();
+  @Output('processing') DZ_PROCESSING = new EventEmitter<any>();
 
-  @Output('drop'                  ) DZ_DROP                     = new EventEmitter<any>();
-  @Output('dragStart'             ) DZ_DRAGSTART                = new EventEmitter<any>();
-  @Output('dragEnd'               ) DZ_DRAGEND                  = new EventEmitter<any>();
-  @Output('dragEnter'             ) DZ_DRAGENTER                = new EventEmitter<any>();
-  @Output('dragOver'              ) DZ_DRAGOVER                 = new EventEmitter<any>();
-  @Output('dragLeave'             ) DZ_DRAGLEAVE                = new EventEmitter<any>();
+  @Output('drop') DZ_DROP = new EventEmitter<any>();
+  @Output('dragStart') DZ_DRAGSTART = new EventEmitter<any>();
+  @Output('dragEnd') DZ_DRAGEND = new EventEmitter<any>();
+  @Output('dragEnter') DZ_DRAGENTER = new EventEmitter<any>();
+  @Output('dragOver') DZ_DRAGOVER = new EventEmitter<any>();
+  @Output('dragLeave') DZ_DRAGLEAVE = new EventEmitter<any>();
 
-  @Output('thumbnail'             ) DZ_THUMBNAIL                = new EventEmitter<any>();
-  @Output('addedFile'             ) DZ_ADDEDFILE                = new EventEmitter<any>();
-  @Output('addedFiles'            ) DZ_ADDEDFILES               = new EventEmitter<any>();
-  @Output('removedFile'           ) DZ_REMOVEDFILE              = new EventEmitter<any>();
-  @Output('uploadProgress'        ) DZ_UPLOADPROGRESS           = new EventEmitter<any>();
-  @Output('maxFilesReached'       ) DZ_MAXFILESREACHED          = new EventEmitter<any>();
-  @Output('maxFilesExceeded'      ) DZ_MAXFILESEXCEEDED         = new EventEmitter<any>();
+  @Output('thumbnail') DZ_THUMBNAIL = new EventEmitter<any>();
+  @Output('addedFile') DZ_ADDEDFILE = new EventEmitter<any>();
+  @Output('addedFiles') DZ_ADDEDFILES = new EventEmitter<any>();
+  @Output('removedFile') DZ_REMOVEDFILE = new EventEmitter<any>();
+  @Output('uploadProgress') DZ_UPLOADPROGRESS = new EventEmitter<any>();
+  @Output('maxFilesReached') DZ_MAXFILESREACHED = new EventEmitter<any>();
+  @Output('maxFilesExceeded') DZ_MAXFILESEXCEEDED = new EventEmitter<any>();
 
-  @Output('errorMultiple'         ) DZ_ERRORMULTIPLE            = new EventEmitter<any>();
-  @Output('successMultiple'       ) DZ_SUCCESSMULTIPLE          = new EventEmitter<any>();
-  @Output('sendingMultiple'       ) DZ_SENDINGMULTIPLE          = new EventEmitter<any>();
-  @Output('canceledMultiple'      ) DZ_CANCELEDMULTIPLE         = new EventEmitter<any>();
-  @Output('completeMultiple'      ) DZ_COMPLETEMULTIPLE         = new EventEmitter<any>();
-  @Output('processingMultiple'    ) DZ_PROCESSINGMULTIPLE       = new EventEmitter<any>();
+  @Output('errorMultiple') DZ_ERRORMULTIPLE = new EventEmitter<any>();
+  @Output('successMultiple') DZ_SUCCESSMULTIPLE = new EventEmitter<any>();
+  @Output('sendingMultiple') DZ_SENDINGMULTIPLE = new EventEmitter<any>();
+  @Output('canceledMultiple') DZ_CANCELEDMULTIPLE = new EventEmitter<any>();
+  @Output('completeMultiple') DZ_COMPLETEMULTIPLE = new EventEmitter<any>();
+  @Output('processingMultiple') DZ_PROCESSINGMULTIPLE = new EventEmitter<any>();
 
-  @Output('reset'                 ) DZ_RESET                    = new EventEmitter<any>();
-  @Output('queueComplete'         ) DZ_QUEUECOMPLETE            = new EventEmitter<any>();
-  @Output('totalUploadProgress'   ) DZ_TOTALUPLOADPROGRESS      = new EventEmitter<any>();
+  @Output('reset') DZ_RESET = new EventEmitter<any>();
+  @Output('queueComplete') DZ_QUEUECOMPLETE = new EventEmitter<any>();
+  @Output('totalUploadProgress') DZ_TOTALUPLOADPROGRESS = new EventEmitter<any>();
 
   constructor(private zone: NgZone, private renderer: Renderer2, private elementRef: ElementRef,
-    private differs: KeyValueDiffers, @Inject(PLATFORM_ID) private platformId: Object,
-    @Optional() @Inject(DROPZONE_CONFIG) private defaults: DropzoneConfigInterface)
-  {
-    if (isPlatformBrowser(this.platformId)) {
-      import('dropzone').then(dropzone => {
-        this.Dropzone = dropzone;
-        this.Dropzone.autoDiscover = false;
-      });
-    }
+              private differs: KeyValueDiffers, @Inject(PLATFORM_ID) private platformId: Object,
+              @Optional() @Inject(DROPZONE_CONFIG) private defaults: DropzoneConfigInterface) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    const dz = Dropzone
+    dz.autoDiscover = false;
   }
 
   ngOnInit(): void {
-    if (!isPlatformBrowser(this.platformId)) {
+    if (!this.isBrowser) {
       return;
     }
 
@@ -84,7 +101,7 @@ export class DropzoneDirective implements OnInit, OnDestroy, DoCheck, OnChanges 
       (params.maxFiles === 1) ? 'dz-multiple' : 'dz-single');
 
     this.zone.runOutsideAngular(() => {
-      this.instance = new this.Dropzone(this.elementRef.nativeElement, params);
+      this.instance = new Dropzone(this.elementRef.nativeElement, params);
     });
 
     if (this.disabled) {
